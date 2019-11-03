89c33cd7908c00042c9e49cc61d5f87a4e721324
Bryson Creighton <brysoncreighton@gmail.com>
2019-11-03 16:39:42 -0700

    added general page HTML layout, created basic JS functions to test application loading.

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function getDogImages(dogs){
+    console.log('generating dog images...');
+}
+
+function formListener(){
+    console.log('listening for form submission...');
+}
+
+$(function() {
+    console.log('Application has loaded.');
+    formListener();
+})
